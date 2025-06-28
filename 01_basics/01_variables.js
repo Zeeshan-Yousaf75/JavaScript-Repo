1c33f09adbf26467b59d729078438d234d752eb3
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-06-28 15:25:34 +0500

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234
+let accountEmail = "shan@gail.com"
+var accountPassword = "1595"
+accountCity = "Rawalpindi"
+
+let accountState;
+
+accountEmail = "zeesahn@gmsil.com"
+accountPassword = "1122"
+accountCity = "Kohat"
+
+/* Prefer not to use Var 
+because of issue in Block and  Functional Scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all Js code as newer version
+
+//alert (2 + 2) //we are using nodejs , not browser
+
+console.log(4 +
+    3) // code readability should be high
+
+//Premitive Types
+
+let name = "Zeeshan" 
+let age = 23
+let loggedIn = false
+let state;
+
+//Number => 2 to power 53
+//bigInt
+//boolean => true/false
+//null => Standalone vale
+//undefined => value are not assign right now
+//symbol => unique
+
+
+//Objects
+
+console.log(typeof undefined) // undifined
+console.log(typeof null) // object

98327f85dab11ce2509063e668914e74ff321e9a
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-06-28 17:09:19 +0500

    ConversionOpersOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,7 +23,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 22
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+// ********************* Operations ********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "Zeeshan"
+let str2 = " Yousaf"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter)
+
+// prefix  and postfix
 

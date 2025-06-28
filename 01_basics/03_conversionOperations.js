d1541e514a69ff993e8fdcf561f7574086da9d5a
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-06-28 16:13:58 +0500

    ConversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = false
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "22" => 22
+// "22abc" => NaN , but type of NaN is Number
+// true => 1; false => 0
+
+let isLoggedIn = "shan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true;  0 => false
+// "" => false
+// "Shan" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
